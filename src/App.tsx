db40a2545dd828957fc2e77c1171b19ef1f36cdd
jrdev <astronomi@gmail.com>
2021-07-31 15:34:29 -0300

    Updating files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello World!</h1>
     </div>
   );
