12fc3b371fc8ab59f95bb0b4d4380f94c2ea0356
jrdev <astronomi@gmail.com>
2021-07-31 21:25:52 -0300

    Finished header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,12 @@ import Styles from './styles';
 export function Header() {
   return (
     <Styles.Container>
-      <img src={logoImage} alt="Personal Finance" />
-      <button type="button" title="New Transaction">
-        Nova transação
-      </button>
+      <Styles.Content>
+        <img src={logoImage} alt="Personal Finance" />
+        <button type="button" title="New Transaction">
+          Nova transação
+        </button>
+      </Styles.Content>
     </Styles.Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,4 +5,37 @@ export default {
   Container: styled.header`
     background: var(--blue);
   `,
+
+  Content: styled.div`
+    max-width: 70%;
+    margin: 0 auto;
+
+    padding: 2rem 1rem 12rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+      width: 250px;
+    }
+
+    button {
+      font-size: 1rem;
+      color: #fff;
+      background: var(--blue-light);
+      border: 0;
+      padding: 0 2rem;
+      border-radius: 0.50rem;
+      height: 3rem;
+
+      transition: all 2s ease;
+
+      &:hover {
+        filter: contrast(9);
+        background: var(--background);
+        color: var(--blue-light);
+      }
+    }
+  `,
+
 }
