e767c069bcd5c6fe8dad46bb00de5dc60c4b642f
jrdev <astronomi@gmail.com>
2021-08-04 20:46:05 -0300

    Passing parameter onOpenNewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
 import logoImage from '../../assets/logo.png';
 import Styles from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  
   return (
     <Styles.Container>
       <Styles.Content>
         <img src={logoImage} alt="Personal Finance" />
-        <button type="button" title="New Transaction">
+        <button 
+          type="button" 
+          title="New Transaction" 
+          onClick={onOpenNewTransactionModal}
+        >
           Nova transação
         </button>
+
+        
       </Styles.Content>
     </Styles.Container>
   )
