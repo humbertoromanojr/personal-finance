1189435ffdc91c85993512f9b747dc4567f85e0c
jrdev <astronomi@gmail.com>
2021-08-04 21:24:00 -0300

    Passing parameters and Create NewTransactionModal

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import styled from 'styled-components';
+
+export default {
+  Container: styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+    margin-top: -7rem;
+  `
+}
