9c9623bddedc1c7f9246ba413d48413f6a0dea70
jrdev <astronomi@gmail.com>
2021-08-07 21:49:33 -0300

    Save form data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -13,9 +13,14 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   const [type, setType] = useState('deposit');
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
 
   function handleCreateTransaction(event: FormEvent) {
     event.preventDefault();
+
+    console.log({title, value, category, type})
   }
 
   return (
@@ -33,8 +38,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <Styles.Container onSubmit={handleCreateTransaction}>
           <h2>Cadastrar Transação</h2>
 
-          <input placeholder="Título" />
-          <input type="number" placeholder="Valor" />
+          <input 
+            placeholder="Título"
+            value={title} 
+            onChange={event => setTitle(event.target.value)}
+          />
+          <input 
+            type="number" 
+            placeholder="Valor"
+            value={value} 
+            onChange={event => setValue(Number(event.target.value))}
+          />
 
           <Styles.TransactionTypeContainer>
             <Styles.RadioBox 
@@ -58,7 +72,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             </Styles.RadioBox>
           </Styles.TransactionTypeContainer>
 
-          <input placeholder="Categoria" />
+          <input 
+            placeholder="Categoria" 
+            value={category} 
+            onChange={event => setCategory(event.target.value)}
+          />
           
           <button type="submit">Cadastrar</button>
         </Styles.Container>
