b97cfe12ee7aaa83a9055c13f4146db9c1f00bde
jrdev <astronomi@gmail.com>
2021-08-28 15:59:45 -0300

    Adding start list

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,68 @@ import { App } from './App';
 
 createServer({
   models: {
-    transition: Model,
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Rental',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 500,
+          createdAt: new Date('2021-08-10 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Internet',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 150,
+          createdAt: new Date('2021-08-10 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'NuBank',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 1150,
+          createdAt: new Date('2021-08-22 12:00:00'),
+        },
+        {
+          id: 4,
+          title: 'Freelance de App',
+          type: 'deposit',
+          category: 'Freela',
+          amount: 8700,
+          createdAt: new Date('2021-01-12 09:00:00'),
+        },
+        {
+          id: 5,
+          title: 'Card Vee',
+          type: 'deposit',
+          category: 'Work',
+          amount: 1057,
+          createdAt: new Date('2021-01-12 09:00:00'),
+        },
+      ],
+    })
   },
 
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transaction');      
+      return this.schema.all('transaction')      
     })
 
     this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody);
+      const data = JSON.parse(request.requestBody)
 
       return schema.create('transaction', data)
     })
-
   }
 })
 
